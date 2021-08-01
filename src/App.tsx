7b61d551ee38cda1ab597e331284823d2d4a190f
Gideon Crawley <gscrawley@gmail.com>
2021-08-01 13:42:54 -0700

    successfully compiled flat World Map Atlas and Round World Map

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.scss'
+import RoundWorldMap from './components/WorldMap/RoundWorldMap'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <RoundWorldMap />
+
+      </header>
+    </div>
+  )
+}
+
+export default App
+
+
